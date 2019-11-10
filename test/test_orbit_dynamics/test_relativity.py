433ba18516e22b6fa6c77ed5784cdf0736a0ec7f
Duncan Eddy <duncan.eddy@gmail.com>
2019-11-10 09:32:33 -0800

    Working SRP and relativity force models

--- test/test_orbit_dynamics/test_relativity.py
+++ test/test_orbit_dynamics/test_relativity.py
@@ -1,2 +1,17 @@
 # Test Imports
+import pytest
 from pytest import approx
+import math
+import numpy as np
+
+# Modules Under Test
+from brahe.epoch import Epoch
+import brahe.astro as _astro
+import brahe.orbit_dynamics.relativity as _rel
+
+def test_accel_relativity(state_keplerian_deg):
+    a_rel = _rel.accel_relativity(_astro.sOSCtoCART(state_keplerian_deg, use_degrees=True))
+
+    assert 0.0 < math.fabs(a_rel[0]) < 1.0e-7
+    assert 0.0 < math.fabs(a_rel[1]) < 1.0e-7
+    assert 0.0 < math.fabs(a_rel[2]) < 1.0e-7

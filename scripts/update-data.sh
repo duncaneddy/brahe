9aba98741d94871bfdd76ad5f3bd87f292c6e295
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-02 09:29:32 -0800

    fix: Fix path error in update script from module refactor

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -9,9 +9,9 @@
 # 
 
 # IERS Earth Orientation Data
-curl -L https://datacenter.iers.org/data/latestVersion/finals.all.iau2000.txt -o ./lib/data/finals.all.iau2000.txt
-curl -L https://datacenter.iers.org/data/latestVersion/EOP_20_C04_one_file_1962-now.txt -o ./lib/data/EOP_20_C04_one_file_1962-now.txt
+curl -L https://datacenter.iers.org/data/latestVersion/finals.all.iau2000.txt -o ./data/finals.all.iau2000.txt
+curl -L https://datacenter.iers.org/data/latestVersion/EOP_20_C04_one_file_1962-now.txt -o ./data/EOP_20_C04_one_file_1962-now.txt
 
 # Space Weather Data
-curl -L https://celestrak.com/SpaceData/sw19571001.txt -o ./lib/data/sw19571001.txt
-curl -L ftp://ftp.seismo.nrcan.gc.ca/spaceweather/solar_flux/daily_flux_values/fluxtable.txt -o ./lib/data/fluxtable.txt
+curl -L https://celestrak.com/SpaceData/sw19571001.txt -o ./data/sw19571001.txt
+curl -L ftp://ftp.seismo.nrcan.gc.ca/spaceweather/solar_flux/daily_flux_values/fluxtable.txt -o ./data/fluxtable.txt

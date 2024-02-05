0aa9c6fd5b9d46d3db21886367d7721a4501765c
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-05 08:27:10 -0800

    Remove: Remove time 'Duration' struct as it is wasn't integrated into rest of module

--- src/time/mod.rs
+++ src/time/mod.rs
@@ -3,13 +3,11 @@
  */
 
 pub mod conversions;
-pub mod duration;
 pub mod epoch;
 pub mod time_range;
 pub mod time_types;
 
 pub use conversions::*;
-pub use duration::*;
 pub use epoch::*;
 pub use time_range::*;
 pub use time_types::*;

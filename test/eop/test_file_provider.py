af1642615c19ee5869a7f9818d61927cd3f1c219
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-05 17:04:08 -0800

    WIP: Finish implementing python EOP tests

--- test/eop/test_file_provider.py
+++ test/eop/test_file_provider.py
@@ -3,52 +3,52 @@ import brahe
 def test_from_c04_file(iau2000_c04_20_filepath):
     eop = brahe.FileEOPProvider.from_c04_file(iau2000_c04_20_filepath, True, "Hold")
 
-    # assert brahe.get_global_eop_initialization() == True
-    # assert brahe.get_global_eop_len() == 21877
-    # assert brahe.get_global_eop_type() == "C04"
-    # assert brahe.get_global_eop_extrapolate() == "Zero"
-    # assert brahe.get_global_eop_interpolate() == True
-    # assert brahe.get_global_eop_mjd_min() == 37665
-    # assert brahe.get_global_eop_mjd_max() == 59541
-    # assert brahe.get_global_eop_mjd_last_lod() == 59541
-    # assert brahe.get_global_eop_mjd_last_dxdy() == 59541
+    assert eop.is_initialized() == True
+    assert eop.len() == 22605
+    assert eop.eop_type() == "C04"
+    assert eop.extrapolation() == "Hold"
+    assert eop.interpolation() == True
+    assert eop.mjd_min() == 37665.0
+    assert eop.mjd_max() == 60269.0
+    assert eop.mjd_last_lod() == 60269.0
+    assert eop.mjd_last_dxdy() == 60269.0
 
 def test_from_default_c04():
-    eop = brahe.FileEOPProvider.from_default_c04(True, "Zero")
+    eop = brahe.FileEOPProvider.from_default_c04(False, "Zero")
 
-    # assert brahe.get_global_eop_initialization() == True
-    # assert brahe.get_global_eop_len() == 21877
-    # assert brahe.get_global_eop_type() == "C04"
-    # assert brahe.get_global_eop_extrapolate() == "Zero"
-    # assert brahe.get_global_eop_interpolate() == True
-    # assert brahe.get_global_eop_mjd_min() == 37665
-    # assert brahe.get_global_eop_mjd_max() == 59541
-    # assert brahe.get_global_eop_mjd_last_lod() == 59541
-    # assert brahe.get_global_eop_mjd_last_dxdy() == 59541
+    assert eop.is_initialized() == True
+    assert eop.len() >= 22605
+    assert eop.eop_type() == "C04"
+    assert eop.extrapolation() == "Zero"
+    assert eop.interpolation() == False
+    assert eop.mjd_min() == 37665.0
+    assert eop.mjd_max() >= 60269.0
+    assert eop.mjd_last_lod() >= 60269.0
+    assert eop.mjd_last_dxdy() >= 60269.0
 
 
 def test_from_standard_file(iau2000_standard_filepath):
     eop = brahe.FileEOPProvider.from_standard_file(iau2000_standard_filepath, True, "Hold")
 
-    # assert brahe.get_global_eop_initialization() == True
-    # assert brahe.get_global_eop_len() == 18261
-    # assert brahe.get_global_eop_type() == "StandardBulletinA"
-    # assert brahe.get_global_eop_extrapolate() == "Hold"
-    # assert brahe.get_global_eop_interpolate() == True
-    # assert brahe.get_global_eop_mjd_min() == 41684
-    # assert brahe.get_global_eop_mjd_max() == 59944
-    # assert brahe.get_global_eop_mjd_last_lod() == 59570
-    # assert brahe.get_global_eop_mjd_last_dxdy() == 59648
+    assert eop.is_initialized() == True
+    assert eop.len() == 18989
+    assert eop.eop_type() == "StandardBulletinA"
+    assert eop.extrapolation() == "Hold"
+    assert eop.interpolation() == True
+    assert eop.mjd_min() == 41684.0
+    assert eop.mjd_max() == 60672.0
+    assert eop.mjd_last_lod() == 60298.0
+    assert eop.mjd_last_dxdy() == 60373.0
 
 def test_from_default_standard():
     eop = brahe.FileEOPProvider.from_default_standard(True, "Hold")
 
-    # assert brahe.get_global_eop_initialization() == True
-    # assert brahe.get_global_eop_len() != 0
-    # assert brahe.get_global_eop_type() == "StandardBulletinA"
-    # assert brahe.get_global_eop_extrapolate() == "Hold"
-    # assert brahe.get_global_eop_interpolate() == True
-    # assert brahe.get_global_eop_mjd_min() == 41684
-    # assert brahe.get_global_eop_mjd_max() >= 59944
-    # assert brahe.get_global_eop_mjd_last_lod() >= 59570
-    # assert brahe.get_global_eop_mjd_last_dxdy() >= 59648
+    assert eop.is_initialized() == True
+    assert eop.len() >= 18989
+    assert eop.eop_type() == "StandardBulletinA"
+    assert eop.extrapolation() == "Hold"
+    assert eop.interpolation() == True
+    assert eop.mjd_min() == 41684.0
+    assert eop.mjd_max() >= 60672.0
+    assert eop.mjd_last_lod() >= 60298.0
+    assert eop.mjd_last_dxdy() >= 60373.0

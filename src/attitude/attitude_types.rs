1055aef96380a625312124a48ded7ad784c41cc9
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-06 11:27:50 -0800

    WIP: Attitude representation implementation work

--- src/attitude/attitude_types.rs
+++ src/attitude/attitude_types.rs
@@ -1,1 +1,132 @@
+/*!
+ * Defines the attitude types used in Brahe
+ */
 
+use std::fmt;
+use nalgebra::{Matrix3, Vector3, Vector4};
+
+/// The `Quaternion` struct represents a quaternion in the form of one scalar and three vector components.
+/// The scalar component is the real part of the quaternion, and the vector components are the imaginary
+/// parts. The quaternion is defined as:
+///
+/// `q = [s, v] = [s, v1, v2, v3]`
+///
+/// `Quaternion` structure implements the AttitudeRepresentation trait, which allows for conversion to and from
+/// other attitude representations. Specifically, `EulerAxis`, `EulerAngle`, and `RotationMatrix`.
+#[derive(Clone, Copy)]
+pub struct Quaternion {
+    pub(crate) data: Vector4<f64>,
+}
+
+/// The `EulerAxis` struct represents an Euler Axis in the form of an angle and a vector. The angle is the
+/// rotation about the axis, and the vector is the axis of rotation. The Euler Axis is defined as:
+///
+/// `e = [angle, v] = [angle, v1, v2, v3]`
+///
+/// `EulerAxis` structure implements the AttitudeRepresentation trait, which allows for conversion to and from
+/// other attitude representations. Specifically, `Quaternion`, `EulerAngle`, and `RotationMatrix`.
+#[derive(Clone, Copy)]
+pub struct EulerAxis {
+    pub axis: Vector3<f64>,
+    pub angle: f64,
+}
+
+/// The EulerAngleOrder enum represents the order of the Euler angles in a set of Euler angles. The order
+/// of the angles is important, as it determines the sequence of rotations that are applied to the base coordinate
+/// system to arrive at the final orientation. The EulerAngleOrder enum is used to specify the order of the
+/// angles in the EulerAngles struct.
+///
+/// The EulerAngleOrder enum is used to specify the order of the angles in the EulerAngles struct. The enum
+/// provides a set of constants that represent the 12 possible combinations of the three angles. The constants
+/// are named according to the order of the angles, where the first angle is the first letter, the second angle
+/// is the second letter, and the third angle is the third letter. For example, the constant `EulerAngleOrder::XYZ`
+/// represents the order of rotations where the first rotation is about the x-axis, the second rotation is about
+/// the y-axis, and the third rotation is about the z-axis. This ordering also has an equivalent numerical value
+/// that is used to represent the order in the EulerAngles struct, where `1` represents a rotation about the x-axis,
+/// `2` represents a rotation about the y-axis, and `3` represents a rotation about the z-axis. So `EulerAngleOrder::XYZ`
+/// constant has a numerical value of `123`.
+#[derive(Clone, Copy, PartialEq)]
+pub enum EulerAngleOrder {
+    XYX = 121,
+    XYZ = 123,
+    XZX = 131,
+    XZY = 132,
+    YXY = 212,
+    YXZ = 213,
+    YZX = 231,
+    YZY = 232,
+    ZXY = 312,
+    ZXZ = 313,
+    ZYX = 321,
+    ZYZ = 323,
+}
+
+impl fmt::Display for EulerAngleOrder {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            EulerAngleOrder::XYX => write!(f, "121"),
+            EulerAngleOrder::XYZ => write!(f, "123"),
+            EulerAngleOrder::XZX => write!(f, "131"),
+            EulerAngleOrder::XZY => write!(f, "132"),
+            EulerAngleOrder::YXY => write!(f, "212"),
+            EulerAngleOrder::YXZ => write!(f, "213"),
+            EulerAngleOrder::YZX => write!(f, "231"),
+            EulerAngleOrder::YZY => write!(f, "232"),
+            EulerAngleOrder::ZXY => write!(f, "312"),
+            EulerAngleOrder::ZXZ => write!(f, "313"),
+            EulerAngleOrder::ZYX => write!(f, "321"),
+            EulerAngleOrder::ZYZ => write!(f, "323"),
+        }
+    }
+}
+
+impl fmt::Debug for EulerAngleOrder {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            EulerAngleOrder::XYX => write!(f, "XYX::121"),
+            EulerAngleOrder::XYZ => write!(f, "XYZ::123"),
+            EulerAngleOrder::XZX => write!(f, "XZX::131"),
+            EulerAngleOrder::XZY => write!(f, "XZY::132"),
+            EulerAngleOrder::YXY => write!(f, "YXY::212"),
+            EulerAngleOrder::YXZ => write!(f, "YXZ::213"),
+            EulerAngleOrder::YZX => write!(f, "YZX::231"),
+            EulerAngleOrder::YZY => write!(f, "YZY::232"),
+            EulerAngleOrder::ZXY => write!(f, "ZXY::312"),
+            EulerAngleOrder::ZXZ => write!(f, "ZXZ::313"),
+            EulerAngleOrder::ZYX => write!(f, "ZYX::321"),
+            EulerAngleOrder::ZYZ => write!(f, "ZYZ::323"),
+        }
+    }
+}
+
+/// The `EulerAngle` struct represents an attitude transformation in the form of three successive rotations about
+/// the x, y, or z axes. The Euler angles are defined as:
+///
+/// `e = [phi, theta, psi]`
+///
+/// Where `phi` is first rotation, `theta` is the second rotation, and `psi` is the third rotation. The axis of each
+/// rotation is determined by the order, which is specified by the `EulerAngleOrder` enum field, `order`.
+///
+/// The EulerAngle structure implements the AttitudeRepresentation trait, which allows for conversion to and from
+/// other attitude representations. Specifically, `Quaternion`, `EulerAxis`, and `RotationMatrix`.
+#[derive(Clone, Copy)]
+pub struct EulerAngle {
+    pub order: EulerAngleOrder,
+    pub phi: f64,
+    pub theta: f64,
+    pub psi: f64,
+}
+
+/// The `RotationMatrix` struct represents an attitude transformation in the form of a 3x3 rotation matrix. The
+/// rotation matrix is defined as:
+///
+/// `R = | r11, r12, r13 |`
+/// `    | r21, r22, r23 |`
+/// `    | r31, r32, r33 |`
+///
+/// The RotationMatrix structure implements the AttitudeRepresentation trait, which allows for conversion to and from
+/// other attitude representations. Specifically, `Quaternion`, `EulerAxis`, and `EulerAngle`.
+#[derive(Clone, Copy)]
+pub struct RotationMatrix {
+    pub(crate) data: Matrix3<f64>,
+}
--- src/attitude/euler_axis.rs
+++ src/attitude/euler_axis.rs
@@ -1,1 +1,117 @@
+/*!
+The `euler_axis` module provides the `EulerAxis` struct, which represents an attitude transformation in the form of a
+single rotation about an arbitrary axis.
+*/
 
+use std::{ops, fmt};
+use nalgebra::{Vector3, Vector4};
+
+use crate::constants::{DEG2RAD, RAD2DEG};
+use crate::attitude::ToAttitude;
+use crate::attitude::attitude_types::{Quaternion, EulerAngle, RotationMatrix, EulerAngleOrder, EulerAxis};
+
+impl EulerAxis {
+    pub fn new(axis: Vector3<f64>, angle: f64) -> Self {
+        Self { axis, angle }
+    }
+
+    pub fn from_vector(vector: Vector4<f64>, as_degrees: bool, vector_first: bool) -> Self {
+        let (angle, axis) = if vector_first {
+            (vector[3], Vector3::new(vector[0], vector[1], vector[2]))
+        } else {
+            (vector[0], Vector3::new(vector[1], vector[2], vector[3]))
+        };
+
+        if as_degrees {
+            Self::new(axis, angle * DEG2RAD)
+        } else {
+            Self::new(axis, angle)
+        }
+    }
+
+    pub fn to_vector(&self, as_degrees: bool, vector_first: bool) -> Vector4<f64> {
+        if as_degrees {
+            if vector_first {
+                Vector4::new(self.angle * RAD2DEG, self.axis.x, self.axis.y, self.axis.z)
+            } else {
+                Vector4::new(self.axis.x, self.axis.y, self.axis.z, self.angle * RAD2DEG)
+            }
+        } else {
+            if vector_first {
+                Vector4::new(self.angle, self.axis.x, self.axis.y, self.axis.z)
+            } else {
+                Vector4::new(self.axis.x, self.axis.y, self.axis.z, self.angle)
+            }
+        }
+    }
+}
+
+impl fmt::Display for EulerAxis {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: Accept formatting options per https://doc.rust-lang.org/std/fmt/struct.Formatter.html
+        write!(f, "EulerAxis: [axis: ({}, {}, {}), angle: {}]", self.axis.x, self.axis.y, self.axis.z, self.angle * RAD2DEG)
+    }
+}
+
+impl fmt::Debug for EulerAxis {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "EulerAxis<Axis<{},{},{}>,Angle<{}>>", self.axis.x, self.axis.y, self.axis.z, self.angle * RAD2DEG)
+    }
+}
+
+impl ops::Index<usize> for EulerAxis {
+    type Output = f64;
+
+    fn index(&self, idx: usize) -> &f64 {
+        &self.axis[idx]
+    }
+}
+
+impl PartialEq for EulerAxis {
+    fn eq(&self, other: &Self) -> bool {
+        self.axis == other.axis && self.angle == other.angle
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.axis != other.axis || self.angle != other.angle
+    }
+}
+
+impl ToAttitude for EulerAxis {
+    fn from_quaternion(q: Quaternion) -> Self {
+        todo!()
+    }
+
+    fn from_euler_axis(e: EulerAxis) -> Self {
+        todo!()
+    }
+
+    fn from_euler_angle(e: EulerAngle) -> Self {
+        todo!()
+    }
+
+    fn from_rotation_matrix(r: RotationMatrix) -> Self {
+        todo!()
+    }
+
+    fn to_quaternion(&self) -> Quaternion {
+        todo!()
+    }
+
+    fn to_euler_axis(&self) -> EulerAxis {
+        todo!()
+    }
+
+    fn to_euler_angle(&self, order: EulerAngleOrder) -> EulerAngle {
+        todo!()
+    }
+
+    fn to_rotation_matrix(&self) -> RotationMatrix {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}

7a18071b73be00baa5d47824b3def576ec6bbcd9
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-16 14:38:17 -0800

    feat: Add frame and coordinate conversions CLI

--- brahe/cli/__main__.py
+++ brahe/cli/__main__.py
@@ -2,7 +2,7 @@ import typer
 import brahe.cli.eop as eop
 import brahe.cli.time as time
 import brahe.cli.orbits as orbits
-import brahe.cli.conversions as conversions
+import brahe.cli.convert as convert
 import brahe.cli.space_track as space_track
 import brahe.cli.access as access
 
@@ -10,7 +10,7 @@ app = typer.Typer(name="brahe")
 app.add_typer(eop.app, name="eop")
 app.add_typer(time.app, name="time")
 app.add_typer(orbits.app, name="orbits")
-app.add_typer(conversions.app, name="conversions")
+app.add_typer(convert.app, name="convert")
 app.add_typer(space_track.app, name="space-track")
 app.add_typer(access.app, name="access")
 

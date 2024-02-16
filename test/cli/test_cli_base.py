e539562d65340c1eb51ca6446a3be5002298667f
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-16 13:13:15 -0800

    feat: Implement EOP CLI

---
+++ test/cli/test_cli_base.py
@@ -0,0 +1,40 @@
+from typer.testing import CliRunner
+
+from brahe.cli.__main__ import app
+
+runner = CliRunner()
+
+def test_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_eop_help():
+    result = runner.invoke(app, ["eop", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe eop [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_time_help():
+    result = runner.invoke(app, ["time", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe time [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_orbits_help():
+    result = runner.invoke(app, ["orbits", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe orbits [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_conversions_help():
+    result = runner.invoke(app, ["conversions", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe conversions [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_space_track_help():
+    result = runner.invoke(app, ["space-track", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe space-track [OPTIONS] COMMAND [ARGS]..." in result.stdout
+
+def test_access_help():
+    result = runner.invoke(app, ["access", "--help"])
+    assert result.exit_code == 0
+    assert "Usage: brahe access [OPTIONS] COMMAND [ARGS]..." in result.stdout
---
+++ test/cli/test_cli_eop.py
@@ -0,0 +1,167 @@
+import tempfile
+from unittest.mock import patch
+from typer.testing import CliRunner
+
+from brahe.cli.__main__ import app
+import brahe
+
+runner = CliRunner()
+
+
+
+
+def test_cli_eop_download_standard(mocker):
+    tmpfile = tempfile.NamedTemporaryFile().name
+    with patch("brahe.download_standard_eop_file") as mock:
+        result = runner.invoke(app, ["eop", "download", tmpfile, "--product", "standard"])
+        assert result.exit_code == 0
+        assert f"Downloaded standard EOP data to {tmpfile}" in result.stdout
+        assert mock.called_once_with(tmpfile)
+
+def test_cli_eop_download_c04(mocker):
+    tmpfile = tempfile.NamedTemporaryFile().name
+    with patch("brahe.download_c04_eop_file") as mock:
+        result = runner.invoke(app, ["eop", "download", tmpfile, "--product", "c04"])
+        assert result.exit_code == 0
+        assert f"Downloaded c04 EOP data to {tmpfile}" in result.stdout
+        assert mock.called_once_with(tmpfile)
+
+def test_cli_eop_get_utc_ut1(iau2000_standard_filepath, iau2000_c04_20_filepath):
+
+    # Patch the eop initialization to use the test files
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z"])
+        assert result.exit_code == 0
+        assert '-0.1104988' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z", "--product", "standard"])
+        assert result.exit_code == 0
+        assert '-0.1104988' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z", "--product", "standard", "--source", "file", "--filepath", iau2000_standard_filepath])
+    assert result.exit_code == 0
+    assert '-0.1104988' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '-0.1105073' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '-0.1105073' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-utc-ut1", "2022-01-01T00:00:00Z", "--product", "c04", "--source", "file", "--filepath", iau2000_c04_20_filepath])
+    assert result.exit_code == 0
+    assert '-0.1105073' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-utc-ut1", "3000-01-01T00:00:00Z"])
+    assert result.exit_code == 1
+    assert 'is out of range for EOP data.' in result.stdout
+
+def test_cli_eop_get_polar_motion(iau2000_standard_filepath, iau2000_c04_20_filepath):
+
+    # Patch the eop initialization to use the test files
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z"])
+        assert result.exit_code == 0
+        assert '2.6492158790549485e-07, 1.3428660227580594e-06' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z", "--product", "standard"])
+        assert result.exit_code == 0
+        assert '2.6492158790549485e-07, 1.3428660227580594e-06' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z", "--product", "standard", "--source", "file", "--filepath", iau2000_standard_filepath])
+    assert result.exit_code == 0
+    assert '2.6492158790549485e-07, 1.3428660227580594e-06' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '2.649555248631725e-07, 1.342846630210815e-06' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '2.649555248631725e-07, 1.342846630210815e-06' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-polar-motion", "2022-01-01T00:00:00Z", "--product", "c04", "--source", "file", "--filepath", iau2000_c04_20_filepath])
+    assert result.exit_code == 0
+    assert '2.649555248631725e-07, 1.342846630210815e-06' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-polar-motion", "3000-01-01T00:00:00Z"])
+    assert result.exit_code == 1
+    assert 'is out of range for EOP data.' in result.stdout
+
+def test_cli_eop_get_cip_offset(iau2000_standard_filepath, iau2000_c04_20_filepath):
+
+    # Patch the eop initialization to use the test files
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z"])
+        assert result.exit_code == 0
+        assert '4.6057299705405924e-10, -1.21203420277384e-09' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z", "--product", "standard"])
+        assert result.exit_code == 0
+        assert '4.6057299705405924e-10, -1.21203420277384e-09' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z", "--product", "standard", "--source", "file", "--filepath", iau2000_standard_filepath])
+    assert result.exit_code == 0
+    assert '4.6057299705405924e-10, -1.21203420277384e-09' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '3.1997702953229375e-10, -1.0714382352520745e-09' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '3.1997702953229375e-10, -1.0714382352520745e-09' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-cip-offset", "2022-01-01T00:00:00Z", "--product", "c04", "--source", "file", "--filepath", iau2000_c04_20_filepath])
+    assert result.exit_code == 0
+    assert '3.1997702953229375e-10, -1.0714382352520745e-09' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-cip-offset", "3000-01-01T00:00:00Z"])
+    assert result.exit_code == 1
+    assert 'is out of range for EOP data.' in result.stdout
+
+def test_cli_eop_get_lod(iau2000_standard_filepath, iau2000_c04_20_filepath):
+
+    # Patch the eop initialization to use the test files
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z"])
+        assert result.exit_code == 0
+        assert '-2.6700000000000002e-05' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_standard_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z", "--product", "standard"])
+        assert result.exit_code == 0
+        assert '-2.6700000000000002e-05' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z", "--product", "standard", "--source", "file", "--filepath", iau2000_standard_filepath])
+    assert result.exit_code == 0
+    assert '-2.6700000000000002e-05' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '-3.14e-05' in result.stdout
+
+    with patch("brahe.cli.eop.get_global_eop_source", return_value=brahe.FileEOPProvider.from_file(iau2000_c04_20_filepath, True, "Error")) as mock:
+        result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z", "--product", "c04"])
+        assert result.exit_code == 0
+        assert '-3.14e-05' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-lod", "2022-01-01T00:00:00Z", "--product", "c04", "--source", "file", "--filepath", iau2000_c04_20_filepath])
+    assert result.exit_code == 0
+    assert '-3.14e-05' in result.stdout
+
+    result = runner.invoke(app, ["eop", "get-lod", "3000-01-01T00:00:00Z"])
+    assert result.exit_code == 1
+    assert 'is out of range for EOP data.' in result.stdout

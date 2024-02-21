f3287d6cce3b875bb1c8e68b5c5134e8a734692d
Duncan Eddy <duncan.eddy@gmail.com>
2024-02-21 07:44:23 -0800

    WIP: Progress getting numerical integration working

--- src/integrators/mod.rs
+++ src/integrators/mod.rs
@@ -1,1 +1,11 @@
-mod numerical_integrator;
+/*!
+ This module contains implementation of numerical integrators.
+*/
+
+pub mod numerical_integrator;
+pub mod runge_kutta;
+pub mod butcher_tableau;
+
+pub use numerical_integrator::*;
+pub use runge_kutta::*;
+pub use butcher_tableau::*;

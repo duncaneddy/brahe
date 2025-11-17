a65ac3881069e8ce28c008fad4aaf54c745078a2
Duncan Eddy <duncan.eddy@gmail.com>
2025-11-17 03:12:23 -0800

    Separate atmospheric density models module from drag model module

--- src/orbit_dynamics/mod.rs
+++ src/orbit_dynamics/mod.rs
@@ -3,6 +3,7 @@ Module implementing orbit dynamics models.
  */
 
 pub use drag::*;
+pub use atmospheric_density_models::*;
 pub use ephemerides::*;
 pub use gravity::*;
 pub use relativity::*;
@@ -10,6 +11,7 @@ pub use solar_radiation_pressure::*;
 pub use third_body::*;
 
 pub mod drag;
+pub mod atmospheric_density_models;
 pub mod ephemerides;
 pub mod gravity;
 pub mod relativity;
